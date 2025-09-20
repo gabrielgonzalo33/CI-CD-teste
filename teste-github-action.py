b0c35983146ea211f5c998d0cd5c05e91d94c95c
Gabriel Gonzalo <82288039+gabrielgonzalo33@users.noreply.github.com>
2025-09-20 10:51:32 -0300

    modulo-certo-datetime

--- teste-github-action.py
+++ teste-github-action.py
@@ -2,8 +2,9 @@ import datetime
 
 def main():
     print("Hello, World!")
-    print('Current time: ', datetime.datatime.now())
+    print('Current time: ', datetime.datetime.now())
 
 if __name__=='__main__':
 
     main()
+
